a9238fa66baf5a12e0778b46f1e1817d6038ad87
hatobus <s1240056@gmail.com>
2020-04-27 20:14:26 +0900

    restored with media urls test

--- shellgei_test.go
+++ shellgei_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"encoding/base64"
 	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 	"time"
@@ -84,6 +86,44 @@ func TestRunCmd(t *testing.T) {
 			t.Errorf("got %+v, expected length 1", imgs)
 		} else if got, expected := imgs[0], base64.StdEncoding.EncodeToString([]byte("goodbye\n")); got != expected {
 			t.Errorf("got %v, expected %v", got, expected)
+		}
+	})
+
+	t.Run("with media urls", func(t *testing.T) {
+		urls := []string{"a.png", "b.png"}
+		mux := http.NewServeMux()
+		for _, url := range urls {
+			url := url
+			mux.HandleFunc(
+				"/"+url,
+				func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(http.StatusOK)
+					w.Write([]byte("hello-" + url))
+				},
+			)
+		}
+		ts := httptest.NewServer(mux)
+		defer ts.Close()
+		for i := range urls {
+			urls[i] = ts.URL + "/" + urls[i]
+		}
+
+		out, imgs, err := runCmd("cat /media/0 /media/1", urls, botConfig{
+			DockerImage: dockerImage,
+			Workdir:     "/tmp",
+			Memory:      "100M",
+			MediaSize:   250,
+			Timeout:     time.Duration(20 * time.Second),
+			Tags:        []string{},
+		})
+		if err != nil {
+			t.Fatalf("unexpected error, %v", err)
+		}
+		if expected := "hello-a.pnghello-b.png"; out != expected {
+			t.Errorf("got %q, expected %v", out, expected)
+		}
+		if len(imgs) != 0 {
+			t.Errorf("got %+v, expected empty", imgs)
 		}
 	})
 
